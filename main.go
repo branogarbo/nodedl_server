4ad255860bf42ad4af3978d08b39f60f149b9897
Brian Longmore <brianl.ext@gmail.com>
2021-09-25 09:36:47 -1000

    looking for private ip not router ip lol, get private ip in code

--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"log"
+	"net"
+	"os"
+	"path"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	addrs, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var privateIP string
+
+	for _, a := range addrs {
+		if !a.IsLoopback() && a.To4() != nil {
+			privateIP = a.String()
+			break
+		}
+	}
+
 	s := fiber.New(fiber.Config{
 		Prefork: true,
 		GETOnly: true,
@@ -14,9 +35,10 @@ func main() {
 
 	s.Get("/dl/:file", func(c *fiber.Ctx) error {
 		file := c.Params("file")
+		path := path.Join("dls", file)
 
-		return c.SendFile("./dls/" + file)
+		return c.SendFile(path)
 	})
 
-	log.Fatal(s.Listen("172.20.10.4:3000"))
+	log.Fatal(s.Listen(privateIP + ":3000"))
 }
