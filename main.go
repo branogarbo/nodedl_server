eda303fcc8adadc1319e8df59cba3cb5c4dfcc92
Brian Longmore <brianl.ext@gmail.com>
2021-09-26 12:16:56 -1000

    remove unnecessary net code

--- main.go
+++ main.go
@@ -2,34 +2,12 @@ package main
 
 import (
 	"log"
-	"net"
-	"os"
 	"path"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	host, err := os.Hostname()
-	if err != nil {
-		log.Fatal(err)
-	}
-	addrs, err := net.LookupIP(host)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var privateIP string
-
-	for _, a := range addrs {
-		if !a.IsLoopback() && a.To4() != nil {
-			privateIP = a.String()
-			break
-		}
-	}
-
-	//////////////////////////////////////////////
-
 	s := fiber.New(fiber.Config{
 		Prefork: true,
 		GETOnly: true,
@@ -46,5 +24,5 @@ func main() {
 		return c.SendStatus(fiber.StatusNotFound)
 	})
 
-	log.Fatal(s.Listen(privateIP + ":3000"))
+	log.Fatal(s.Listen(":3000"))
 }
